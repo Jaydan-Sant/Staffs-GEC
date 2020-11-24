80d7a2044c6759cabe32ca4845f69183de285d8a
JaydanSant <59823495+JaydanSant@users.noreply.github.com>
2020-11-24 12:02:29 +0000

    Loops

--- Program16/Program16/Program16.cpp
+++ Program16/Program16/Program16.cpp
@@ -3,9 +3,29 @@ using namespace std;
 
 int main()
 {
+    int damage{};
     int playerLife = 100;
+    
+    
 
     cout << "You have " << playerLife << " HP" << endl;
-    cout << ""
+    
+    for (int i = 1; i < 10; i++)
+    {
+        cout << "Question " << i << ": How much damage should I deal?\n";
+        cin >> damage;
+        playerLife - damage;
+        if (playerLife <= 0)
+        {
+            cout << "Early end. Player Died!\n";
+            break;
+        }
+        else
+        {
+            cout << "You have " << playerLife << " HP\n";
+            continue;
+        }
+    }
+    cout << "All questions asked. Player survived!\n";
 }
 
